552458fd3d5aa6bd069ed53cfb85f2585336fa7c
Panawat <pw443@cam.ac.uk>
2020-04-22 15:03:31 +0700

    add code for bulk categorising 2galaxy problems

---
+++ multiple_csv.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+# from numpy import linspace
+#import multiprocessing
+
+
+# def run_process(process):
+#     print('start', process, multiprocessing.current_process())
+#     command = f'python -m tidaltailsim --nogui -v 2galaxy -n 9999 {process[2]} {process[1]} -o {process[0]} 2000 10 0'
+#     print(f'run {command}')
+#     # os.system(command)
+#     print('end', process)
+
+
+def export_csv(process):
+    command = f'python3.8 -m tidaltailsim --nogui -v categorise_2galaxy_fromfile -f 5499 out/{process[0]}.pkl out_csv/{process[0]}.csv 1 0.1'
+    os.system(command)
+
+
+if __name__ == '__main__':
+    print('Prepare exporting csv 2galaxy problem')
+
+    m2s = [.25, .5, 1., 2., 4.]
+    outputs_m = ['out_f4_{0:04.0f}', 'out_f2_{0:04.0f}', 'out_1_{0:04.0f}', 'out_x2_{0:04.0f}', 'out_x4_{0:04.0f}']
+
+    radii_space = np.linspace(2, 12, 10 * 4 + 1)
+    test_masses_count_space = radii_space * 100
+    test_masses_count_space = test_masses_count_space.astype(np.dtype(int))
+
+    # processes = list()
+    for m, o in zip(m2s, outputs_m):
+        for r, n in np.nditer((radii_space, test_masses_count_space)):
+            out_str = o.format(r * 100)
+            m_str = f'-m 1. {m:.2f}'
+            g1_str = f'-g1 {r:.2f} {n:d}'
+            process = (out_str, m_str, g1_str)
+            export_csv(process)
+            print(f'{out_str} exported')
+
+    # print('Start parallel processing 2galaxy problem, num ex:', len(processes))
+    # pool = multiprocessing.Pool()
+    # pool.map(run_process, processes)
+    # print('End parallel processing 2galaxy problem')
+    exit()
+# python -m tidaltailsim --nogui -v 2galaxy -n 89999 -g1 5 50 -o out0x2.pkl 1800 10 0
+# python -m tidaltailsim --nogui -v 2galaxy -n 9999 -g1 5 50 -o out0x3.pkl 2000 10 0
